e3744d7c216c60d140a369600966b449c92b9bcc
Jernej Filipcic <j.filipcic@sportradar.org>
2021-06-27 18:43:04 +0200

    first commit

---
+++ src/main/java/eu/remi/pactPresentation/PactPresentationApplication.java
@@ -0,0 +1,13 @@
+package eu.remi.pactPresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/remi/pactPresentation/PactPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.remi.pactPresentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
